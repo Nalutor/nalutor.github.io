1447c6bce6ed8a56be1d6725b55e8ecd9d99a88c
nalutor <550655976@qq.com>
2018-11-12 12:22:26 +0800

    Site updated: 2018-11-12 12:22:25

--- js/src/body.js
+++ js/src/body.js
@@ -2,8 +2,7 @@ $(function () {
     var arr = ['FhG3qDLX6dmL7yrrvs8nS-9PZtoL', 'FsZy4o9A28HxKm4bMPx9c9Noa2__', 'FrWj9ATP3Wip7Tl6xp5A9EIKHxDu', 'FuYxuHq8UIaEHX5DnoErdJSZ4O0H', 'Fih89AFiR9hKgyqEQwwpaz2KLZHJ', 'FvQ-37vTWk97QKlcRfRs7TVQUBJE', 'FjOHYY3YqLLWvggtHwG01XDFgm4m', 'FqiV-loUwkhsbSXtLau2-YgtjBjH', 'FsllVmLcoK1THK7E-5APk4rERsOn', 'FtcFGp1x91mDGf9b8YcrmfGv4uWp'];
     var result = window.matchMedia('(min-width:1200px)');
     var random = parseInt(9 * Math.random());
-    alert(random);
     if (result.matches) {
-        $("body").css("background", "url(http://static.ledouya.com/" + arr.random + ")  50% 50% fixed");
+        $("body").css("background", "url(http://static.ledouya.com/" + arr[random] + ")  50% 50% fixed");
     }
 })
